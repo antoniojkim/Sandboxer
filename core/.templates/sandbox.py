764cdfee65ed6d0a57523f033e715dbd149b7d0a
Antonio Kim <antoniok@antoniojkim.com>
2020-04-15 21:46:05 -0400

    Setup sandboxer as python package

--- core/.templates/sandbox.py
+++ core/.templates/sandbox.py
@@ -1,9 +1,6 @@
-import torch
-import numpy as np
 
 def main():
     pass
-
 
 if __name__ == "__main__":
     main()
---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as file:
+    long_description = file.read()
+
+setuptools.setup(
+    name="sandboxer-antoniojkim",
+    version="0.0.1",
+    author="Antonio J Kim",
+    author_email="contact@antoniojkim.com",
+    description="A fast, lightweight sandbox tool for experimenting with code",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/antoniojkim/Sandboxer",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'sandboxer = core.sandboxer:main'
+        ]
+    }
+)
